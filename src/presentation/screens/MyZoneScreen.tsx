3431d5766d27fb9fd02b46419a1c586719618c46
codehong <hsw0323@naver.com>
2026-03-26 13:05:53 +0900

    - navermap 설치

--- src/presentation/screens/MyZoneScreen.tsx
+++ src/presentation/screens/MyZoneScreen.tsx
@@ -1,11 +1,21 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { NaverMapView } from '@mj-studio/react-native-naver-map';
+
+// 초기 카메라 위치 설정
+const INITIAL_CAMERA = {
+  latitude: 37.5666102, // 서울 중심부 위도
+  longitude: 126.9783881, // 서울 중심부 경도
+  zoom: 12, // 줌 레벨
+};
 
 function MyZoneScreen() {
   return (
-    <SafeAreaView>
-      <Text>MyZoneScreen</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
+        <NaverMapView style={{ flex: 1 }} initialCamera={INITIAL_CAMERA} />
+      </View>
     </SafeAreaView>
   );
 }
