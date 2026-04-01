3811bb78c951278e700d27e5a9eee619c636b5e1
codehong <hsw0323@naver.com>
2026-04-01 09:40:44 +0900

    - 필요없는 코드 삭제

--- src/presentation/screens/MyZoneScreen.tsx
+++ src/presentation/screens/MyZoneScreen.tsx
@@ -1,21 +1,30 @@
 import { StyleSheet, View } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { NaverMapView } from '@mj-studio/react-native-naver-map';
+import {
+  NaverMapMarkerOverlay,
+  NaverMapView,
+} from '@mj-studio/react-native-naver-map';
 
 const INITIAL_CAMERA = {
-  latitude: 37.5666102,
-  longitude: 126.9783881,
-  zoom: 12,
+  latitude: 37.5715,
+  longitude: 126.9769,
+  zoom: 14,
 };
 
 function MyZoneScreen() {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.container}>
-        <NaverMapView style={styles.container} initialCamera={INITIAL_CAMERA} />
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <NaverMapView
+        style={styles.container}
+        initialCamera={INITIAL_CAMERA}
+        isShowZoomControls={false}
+      >
+        <NaverMapMarkerOverlay latitude={37.5666102} longitude={126.9783881} />
+        <NaverMapMarkerOverlay latitude={37.5796} longitude={126.977} />
+        <NaverMapMarkerOverlay latitude={37.5657} longitude={126.975} />
+      </NaverMapView>
+    </View>
   );
 }
 
